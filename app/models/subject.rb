39af80b5ac02011c72340aa43f1bb3b250faa01e
Matt Hough <matt@hough.tv>
2017-12-28 17:41:21 +1000

    Fix edit subjects so that more than one user can be added.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -14,7 +14,8 @@ class Subject < ApplicationRecord
   end
 
   def user_emails=(user_emails)
-    emails = user_emails.split(',')
+    emails = user_emails.split(', ')
+    users.clear 
     emails.each do |email|
       user = User.find_by(email: email)
       users << user if user
